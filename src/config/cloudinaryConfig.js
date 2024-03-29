e833933d9999f545de97ec426ce6704ba04b688b
bangbold <bilalimranuw@gmail.com>
2024-03-29 19:58:24 +0400

    Cloudniary 3

--- src/config/cloudinaryConfig.js
+++ src/config/cloudinaryConfig.js
@@ -3,8 +3,15 @@
 const cloudinary = require('cloudinary').v2;
 
 cloudinary.config({ 
+  cloud_name: 'dupqwe9dp', 
+  api_key: '691928634239212', 
+  api_secret: 'O_0DL7h5BVHlKNfGfe8bHgPnPfg' 
 });
 
 module.exports = {
   cloudinary,
 };
+
+module.exports={
+    cloudinary,
+}
